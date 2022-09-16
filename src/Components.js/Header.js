b5a81d79589f974a2ee262510e5bec1878aa1d78
AhmedDesouki <ahmed195141@bue.edu.eg>
2022-09-16 16:20:26 +0200

    My practice react project freecodecamp

---
+++ src/Components.js/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header className="header">
+        <img  
+        src="./Images/Troll Face.png"
+        className="header--image"
+        
+        />
+         <h4 className="header--title">Meme Generator </h4>
+        <h4 className="header--project">React Course - project 3 </h4>
+
+        </header>
+    )
+}
